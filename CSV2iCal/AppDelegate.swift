89bf74efd1945c75ccb0b2f848f5e46dd4385f57
David Ackerman <david.s.ackerman@gmail.com>
2014-11-01 23:44:42 -0600

    Move event creation to only occur on button click.

--- CSV2iCal/AppDelegate.swift
+++ CSV2iCal/AppDelegate.swift
@@ -13,22 +13,12 @@ import EventKit
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 	@IBOutlet weak var window: NSWindow!
-
+	var eventStore = EKEventStore()
 
 	func applicationDidFinishLaunching(aNotification: NSNotification) {
 		// Insert code here to initialize your application
-		var eventStore = EKEventStore()
 		eventStore.requestAccessToEntityType(EKEntityTypeEvent, completion:{ granted, error in
 			print("access granted")
-			var event = EKEvent(eventStore:eventStore)
-			var writeError: NSError?
-			event.title = "Testing"
-			event.startDate = NSDate()
-			event.endDate = event.startDate.dateByAddingTimeInterval(3600)
-			//		var calendar = EKCalendar()
-			event.calendar = eventStore.defaultCalendarForNewEvents
-			eventStore.saveEvent(event, span: EKSpanThisEvent, commit: true, error: &writeError)
-			print(writeError)
 		})
 	}
 
@@ -36,6 +26,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		// Insert code here to tear down your application
 	}
 
-
+	@IBAction func addEvent(AnyObject) {
+		var event = EKEvent(eventStore:eventStore)
+		var writeError: NSError?
+		event.title = "Testing"
+		event.startDate = NSDate()
+		event.endDate = event.startDate.dateByAddingTimeInterval(3600)
+		//		var calendar = EKCalendar()
+		event.calendar = eventStore.defaultCalendarForNewEvents
+		eventStore.saveEvent(event, span: EKSpanThisEvent, commit: true, error: &writeError)
+		print(writeError)
+	}
 }
 
